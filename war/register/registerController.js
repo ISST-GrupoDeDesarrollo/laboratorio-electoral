f1432818caacb6a7901af245003add5ecf964e4d
Ignacio <Ignacio@IgnacioIba>
2016-04-05 16:30:16 +0200

    registerController y login

--- war/register/registerController.js
+++ war/register/registerController.js
@@ -1,6 +1,5 @@
 Laboratory.controller('registerController', ['$scope', '$http', '$routeParams', '$location', function($scope,$http,$routeParams,$location){
-		//register controller code goes here
-		//$scope.controllerName = "registerController";
+		
 	$scope.usuario;
 	$scope.contraseña;
 	$scope.nombreCompleto;
