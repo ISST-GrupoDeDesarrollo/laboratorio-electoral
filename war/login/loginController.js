434de1095f9d75b695e83679f40e333605323fd2
Eduardo <Eduardo@Edu-PC>
2016-04-12 18:40:18 +0200

    Frontend Register Login

--- war/login/loginController.js
+++ war/login/loginController.js
@@ -1,4 +1,33 @@
-Laboratory.controller('loginController', ['$scope', '$http','$routeParams', '$location', function($scope,$http,$routeParams,$location){
-	//login controller code goes here
-	$scope.controllerName = "loginController";
+angular.module('Laboratory', []).controller('loginController', ['$scope', '$http', function($scope,$http){
+  console.log("1");
+
+  $scope.sendLogin = ['$scope', "$http" , function($scope, $http){
+    console.log("2");
+
+    $http({
+      method: "GET",
+      url: "adress",
+      data: {
+        username: $scope.usuario,
+        password: $scope.contraseña
+      }/*,
+      transformRequest: function(data, headersGetter){
+        var formData = new FormData();
+        angular.forEach(data, function(value, key){
+          formData.append(key,value);
+        });
+        var headers = headersGetter();
+        delete headers['Content-Type'];
+        return formData;
+      }*/
+    })
+    .success(function(data){
+
+    })
+    .error(function(data, status){
+
+    });
+
+
+  }];
 }]);
