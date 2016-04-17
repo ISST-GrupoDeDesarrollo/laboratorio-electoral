57d1814746eb07368f874066f61d2e635d8c7454
abenitoc <vermibeto@gmail.com>
2016-04-18 01:39:25 +0200

    Now login works, access to database properly and does json and everything, we could definitely say this work, fuck edu

--- war/login/loginController.js
+++ war/login/loginController.js
@@ -1,11 +1,9 @@
 Laboratory.controller('loginController', ['$scope', '$http','$routeParams', '$location', function($scope,$http,$routeParams,$location){
-	var user = $scope.usuario;
-	var contr = $scope.contrasena;	
-	
+
 	$scope.sendLogin = function(){
 		$http.post('/api/login',{
-			username: user,
-			password: contr
+			username: $scope.usuario,
+			password: $scope.contrasena
 		}
 	)
 	.success(function(response){
