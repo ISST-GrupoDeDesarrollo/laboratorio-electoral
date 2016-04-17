22521b983e1e1e689276f6a47e3c4627fd5074ea
sirasistant <sirasistant@gmail.com>
2016-04-18 01:40:56 +0200

    Creating create simulation modal

--- war/project/projectController.js
+++ war/project/projectController.js
@@ -1,4 +1,22 @@
-Laboratory.controller('projectController', ['$scope', '$http','$routeParams', '$location', function($scope,$http,$routeParams,$location){
-	//project controller code goes here
-	$scope.controllerName = "projectController";
+Laboratory.controller('projectController', ['$scope', '$http','$routeParams', '$location','$uibModal', function($scope,$http,$routeParams,$location,$uibModal){
+	
+	$scope.createSimulation=function(){
+		var modalInstance = $uibModal.open({
+			animation: $scope.animationsEnabled,
+			templateUrl: 'createSimulationModal.html',
+			controller: 'CreateSimulationController',
+			resolve: {
+			}
+		});
+
+		modalInstance.result.then(function (simulation) {
+			$location.path("/projects/"+$routeParams.projectId+"/simulations/"+simulation.id);
+		}, function () {
+			$log.info('Modal dismissed at: ' + new Date());
+		});
+	};
 }]);
+
+Laboratory.controller('ModalInstanceCtrl', function ($scope, $uibModalInstance, items) {
+	
+});
