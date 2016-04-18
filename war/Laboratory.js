80181aafc8d58ab367be9ddf939ab7b7d0b8b421
abenitoc <vermibeto@gmail.com>
2016-04-18 19:16:48 +0200

    VISTAS PROYECTOS: Añadido modificaciones en el nav sign in y en la vista de projects index

--- war/Laboratory.js
+++ war/Laboratory.js
@@ -36,19 +36,3 @@ Laboratory.directive('fileInput', ['$parse', function($parse){
         }
     };
 }]);
-
-/*
-Laboratory.directive('file', function(){
-	return {
-		scope: {
-			file: '='
-		},
-		link: function(scope, element, attrs){
-			element.bind('change', function(event){
-				var file = event.target.files[0];
-				scope.file = file ? file : undefined;
-				scope.$apply();
-			});
-		}
-	};
-});*/
