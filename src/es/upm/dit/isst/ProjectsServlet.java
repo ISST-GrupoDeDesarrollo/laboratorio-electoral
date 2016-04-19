5da50ac7f50202bb42c9da824e0ebd2c351af533
Ignacio <Ignacio@IgnacioIba>
2016-04-19 21:36:05 +0200

    Servlet de proyectos: fin

---
+++ src/es/upm/dit/isst/ProjectsServlet.java
@@ -0,0 +1,18 @@
+package es.upm.dit.isst;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ProjectsServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+	}
+	
+	
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
+	}
+}
