0e04050ddc390a246403e38ee7bdfa7b46da9e6d
Ignacio <Ignacio@IgnacioIba>
2016-04-21 00:36:03 +0200

    Modal view: sin finalizar

--- war/projects/projectsController.js
+++ war/projects/projectsController.js
@@ -2,46 +2,13 @@ Laboratory.controller('projectsController', ['$scope', '$http','$routeParams', '
 	$scope.orderByField = 'name';
 	$scope.reverseSort = false;
 	
-	$scope.showModal = false;
-	$scope.toggleModal = function(){
-		$scope.showModal = !$scope.showModal;
-	};
+	$scope.newProject = function(){
+		console.log("funcion llamada");
+		$modal.open({
+			templateUrl: '/projects/createProjectModal.html'
+		});
+	}
 	
 	
 	
 }]);
-
-
-/*Laboratory.directive('modal', function(){
-	return{
-		template: '/proyects/createProyectModal.html',
-		restrict: 'E',
-		transclude: true,
-		replace: true,
-		scope: true,
-		link: function postLink(scope, element, attrs){
-			$scope.title = attrs.title;
-			
-			$scope.$watch(attrs.visible, function(value){
-				if(value == true){
-					$(element).modal(show);
-				}else{
-					$(element).modal(hide);
-				}
-			});
-			
-			$(element).on('shown.bs.modal', function(){
-				scope.$apply(function(){
-					scope.$parent[attrs.visible] = true;
-				});
-			});
-			
-			$(element).on('hidden.bs.modal', function(){		
-				scope.$apply(function(){
-					scope.$parent[attrs.visible] = false;
-				});
-			});
-		}
-	}
-});
-*/
