4b0407ba05912fcf61e737e12bf995758bf6e61e
sirasistant <sirasistant@gmail.com>
2016-04-20 11:54:50 +0200

    Added project class in model

---
+++ src/es/upm/dit/isst/models/Project.java
@@ -0,0 +1,45 @@
+package es.upm.dit.isst.models;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+import com.google.appengine.datanucleus.annotations.Unowned;
+
+@Entity
+public class Project implements Serializable {
+	
+	@Id
+    @GeneratedValue()
+    private Long id;   
+	
+	private String name;
+	
+	private String description;
+	
+	/*
+	@ManyToOne()
+	@Unowned
+	private Workgroup workgroup;*/
+	 
+	private String party;
+     
+    private int voters;
+     
+    @OneToMany(cascade = CascadeType.REMOVE)
+	@Unowned
+    private List<Simulation> simulations = new ArrayList<>();
+
+	public Long getId() {
+		return id;
+	}
+	
+     
+}
