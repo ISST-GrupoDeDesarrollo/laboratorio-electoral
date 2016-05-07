78bf0a61fa0f9e4cb79f18829c919bae035b94b2
abenitoc <vermibeto@gmail.com>
2016-05-08 00:12:15 +0200

    Results: Added Highcharts to scope of angular and tryed first chart. MAPS ARE COMING GUYS

--- war/result/resultController.js
+++ war/result/resultController.js
@@ -1,4 +1,18 @@
 Laboratory.controller('resultController', ['$scope', '$http','$routeParams', '$location', function($scope,$http,$routeParams,$location){
 	//result controller code goes here
-	$scope.controllerName = "resultController";
+	/**
+	 * Simple highcharts-ng maps example.
+	 */
+	Highcharts.chart('map', {
+
+	    xAxis: {
+	        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 
+	            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+	    },
+
+	    series: [{
+	        data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+	    }]
+	});
+
 }]);
