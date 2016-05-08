dc34047190ec04341d8ec7bc85ebd376abbd5160
sirasistant <sirasistant@gmail.com>
2016-05-08 13:27:57 +0200

    logout improved

--- war/navigationBars/navigationBarController.js
+++ war/navigationBars/navigationBarController.js
@@ -1,5 +1,6 @@
 Laboratory.controller('navigationController', ['$scope', '$http', '$routeParams', '$location', function($scope,$http,$routeParams,$location){
 
+	var reload = function(){
 	      $http.get('/checkLoginServlet').success(function(data) {
 	        
 	        $scope.cleanObjectFromDatabase(data);
@@ -17,13 +18,16 @@ Laboratory.controller('navigationController', ['$scope', '$http', '$routeParams'
 	        })
 	        .error(function(data) {
 	          console.log('error: ' + data);
-	        });
+	        });}
 	        
 	        $scope.logout=function(){
 	        	$http.delete("/api/login",{}).success(function(data,status){
 	        		$location.path("/");
+	        		   reload();
 	        	});
 	        }
+	        
+	        reload();
 
 	    }]);
 	
