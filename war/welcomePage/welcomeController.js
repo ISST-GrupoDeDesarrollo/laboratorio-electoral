4b5bcddf7e52369485ff8e1b4dfeeb1144c4bc8f
sirasistant <sirasistant@gmail.com>
2016-05-09 12:17:18 +0200

    Improved get started

--- war/welcomePage/welcomeController.js
+++ war/welcomePage/welcomeController.js
@@ -1,4 +1,6 @@
 Laboratory.controller('welcomeController', ['$scope', '$http','$routeParams', '$location', function($scope,$http,$routeParams,$location){
-	//Welcome page controller code goes here
-	$scope.controllerName = "welcomeController";
+	
+	$scope.getStarted=function(){
+		$location.path($scope.appUser?"/projects":"/login");
+	}
 }]);
